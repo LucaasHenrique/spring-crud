262bfd5787f127b6f0ba32f1bb429d2153c6f857
LucaasHenrique <lucashsouza101@gmail.com>
2024-07-25 16:14:59 -0300

    put and delete

--- Crud/src/main/java/br/com/Crud/Crud/controllers/ProductController.java
+++ Crud/src/main/java/br/com/Crud/Crud/controllers/ProductController.java
@@ -3,10 +3,14 @@ package br.com.Crud.Crud.controllers;
 import br.com.Crud.Crud.domain.Repository.ProdutosRepository;
 import br.com.Crud.Crud.domain.RequestProduto;
 import br.com.Crud.Crud.domain.entity.Produtos;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
@@ -17,7 +21,7 @@ public class ProductController {
 
     @GetMapping
     public ResponseEntity getAllproducts() {
-        var allproducts = pr.findAll();
+        var allproducts = pr.findAllByActiveTrue();
         return ResponseEntity.ok(allproducts);
     }
 
@@ -30,7 +34,29 @@ public class ProductController {
     }
 
     @PutMapping
-    public ResponseEntity updateProduto() {
-        return ResponseEntity.ok().build();
+    @Transactional
+    public ResponseEntity updateProduto(@RequestBody @Valid RequestProduto data) {
+        Optional<Produtos> produto = pr.findById(data.id());
+        if(produto.isPresent()) {
+            Produtos produto1 = produto.get();
+            produto1.setNome(data.nome());
+            produto1.setPreco_em_centavos(data.preco_em_centavos());
+            return ResponseEntity.ok(produto);
+        } else {
+            throw new EntityNotFoundException();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity deleteProduto(@PathVariable long id) {
+        Optional<Produtos> produto = pr.findById(id);
+        if(produto.isPresent()) {
+            Produtos produto1 = produto.get();
+            produto1.setActive(false);
+            return ResponseEntity.noContent().build();
+        } else {
+            throw new EntityNotFoundException();
+        }
     }
 }
--- Crud/src/main/java/br/com/Crud/Crud/domain/Repository/ProdutosRepository.java
+++ Crud/src/main/java/br/com/Crud/Crud/domain/Repository/ProdutosRepository.java
@@ -3,5 +3,8 @@ package br.com.Crud.Crud.domain.Repository;
 import br.com.Crud.Crud.domain.entity.Produtos;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProdutosRepository extends JpaRepository<Produtos, Long> {
+    List<Produtos> findAllByActiveTrue();
 }
